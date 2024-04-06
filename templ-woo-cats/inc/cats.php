6b30764f2ef74fa34f60e0227004b7d1828599d8
Johan Bouveng <johan.bouveng@gmail.com>
2024-04-06 21:30:43 +0200

    Add files via upload
    Cats for my account.

---
+++ templ-woo-cats/inc/cats.php
@@ -0,0 +1,33 @@
+<?php
+
+	if ( ! defined( 'ABSPATH' ) ) {
+
+		exit;
+
+	}
+
+	function get_cat_api() {
+
+		$api = 'https://cataas.com/cat/says/';
+		$tag = 'TEMPL == FISH';
+		$cat = wp_remote_get( $api . $tag . '?font=Impact&fontSize=30&fontColor=%23fff&type=square&position=center&html=false');
+
+		if ( !is_wp_error( $cat ) ) {
+
+			return base64_encode( wp_remote_retrieve_body( $cat ) );
+
+		}
+	}
+
+
+	add_filter( 'query_vars' , fn( $qv ) => array_merge( $qv, [ 'cats' ] ) );
+	add_filter( 'woocommerce_account_menu_items' , function( $tabs ) {
+
+		$tabs[ 'cats' ] = __( 'Cats' , 'woocommerce' );
+		return $tabs;
+
+	} );
+
+
+	add_action( 'init' , fn() => add_rewrite_endpoint( 'cats' , EP_ROOT | EP_PAGES ) );
+	add_action('woocommerce_account_cats_endpoint', fn() => echo '<img style="width:100%;" src="data:image/jpeg;base64,' . get_cat_api() . '"/><a href="./">Meow…</a>');
---
+++ templ-woo-cats/plugin.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Plugin Name: Templ Woo Cats
+ * Plugin URI: https://github.com/
+ * Description: Displays cats on the WooCommerce MyAccount page.
+ * Author: Templ AB
+ * Author URI: https://templ.io/
+ * Version: 0.0.1
+ */
+
+
+	require_once 'inc/cats.php';
