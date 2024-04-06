e7648888a1a9bda49102d7192b7ff1e9f954c136
Johan Bouveng <johan.bouveng@gmail.com>
2024-04-06 21:32:41 +0200

    Update cats.php

--- templ-woo-cats/inc/cats.php
+++ templ-woo-cats/inc/cats.php
@@ -10,7 +10,7 @@
 
 		$api = 'https://cataas.com/cat/says/';
 		$tag = 'TEMPL == FISH';
-		$cat = wp_remote_get( $api . $tag . '?font=Impact&fontSize=30&fontColor=%23fff&type=square&position=center&html=false');
+		$cat = wp_remote_get( $api . $tag . '?font=Impact&fontSize=30&fontColor=%23fff&type=square&position=center&html=false' );
 
 		if ( !is_wp_error( $cat ) ) {
 
@@ -30,4 +30,4 @@
 
 
 	add_action( 'init' , fn() => add_rewrite_endpoint( 'cats' , EP_ROOT | EP_PAGES ) );
-	add_action('woocommerce_account_cats_endpoint', fn() => echo '<img style="width:100%;" src="data:image/jpeg;base64,' . get_cat_api() . '"/><a href="./">Meow…</a>');
+	add_action( 'woocommerce_account_cats_endpoint' , fn() => echo '<img style="width:100%;" src="data:image/jpeg;base64,' . get_cat_api() . '"/><a href="./">Meow…</a>' );
